5729d66e0533cdd35a473372263c3c910398ee6d
Alexander Tkachev <teksisto@gmail.com>
2013-10-21 13:16:51 +0400

    fix: changed hook which runs _recurrence partial

--- init.rb
+++ init.rb
@@ -7,12 +7,12 @@ Redmine::Plugin.register :redmine_recurring_planning do
   description 'Autoplanning for issues.'
   version '0.0.1'
   url 'https://bitbucket.org/antonovgks/redmine_recurring_planning'
-  author_url 'http://example.com/about'
+  author_url 'http://project.u-k-s.ru/people/479'
   
   project_module :recurring_planning do
     permission :edit_recurring_planning, {:recurring_planning => :index}
   end
-  
+
 end
 
 Rails.configuration.to_prepare do
@@ -24,7 +24,6 @@ Rails.configuration.to_prepare do
   end
 
   [
-    #[IssuesHelper, RecurringPlanningPlugin::IssuesHelperPatch],
     [Issue, RecurringPlanningPlugin::IssuePatch]
   ].each do |cl, patch|
     cl.send(:include, patch) unless cl.included_modules.include? patch
--- lib/redmine_recurring_planning/hooks.rb
+++ lib/redmine_recurring_planning/hooks.rb
@@ -1,5 +1,6 @@
 module RedmineRecurringPlanning
   class Hooks < Redmine::Hook::ViewListener
-    render_on :view_issues_sidebar_issues_bottom, :partial => 'issues/recurrence', :locals => {:issue => @issue}
+    #render_on :view_issues_sidebar_issues_bottom, :partial => 'issues/recurrence', :locals => {:issue => @issue}
+    render_on :view_issues_sidebar_planning_bottom, :partial => 'issues/recurrence', :locals => {:issue => @issue}
   end
 end 
