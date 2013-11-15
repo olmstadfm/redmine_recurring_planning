1eb6f6184c855adfa87c850d146b6cf6a252b930
Alexander Tkachev <teksisto@gmail.com>
2013-11-15 16:38:31 +0400

    add: substitute existing rule values into recurrence_dialog + fixed bug in dialog switching

--- app/helpers/recurring_planning_helper.rb
+++ app/helpers/recurring_planning_helper.rb
@@ -21,22 +21,51 @@ module RecurringPlanningHelper
     res << res.shift
   end
 
+
+  def month_date_checked?(date_number)
+    schedule = @issue.planning_schedule
+    if schedule
+      rule = schedule.recurrence_rules.first.try(:to_hash) || Hash.new
+      if rule[:rule_type] == "IceCube::MonthlyRule"
+        rule[:validations][:day_of_month].include?(date_number) rescue false
+      else
+        false
+      end
+    else
+      false
+    end
+  end
+
   def dates_ul(param_name)
     ('<ul style = "list-style-type: none;">' +
       (1..31).to_a.map{|dt|
-        '<li style="display: inline-block; width: 50px;">' + check_box_tag(param_name, dt) + ' ' + dt.to_s + '</li>'
+        '<li style="display: inline-block; width: 50px;">' + check_box_tag(param_name, dt, month_date_checked?(dt)) + ' ' + dt.to_s + '</li>'
       }.each_slice(7).to_a.
       map{|sept|
         sept.join + '<br>' + '<hr>'
       }.join +
-      '<li style="display: inline-block; width: 200px; margin-top: 10px;">' + check_box_tag(param_name, '-1') + ' ' + l(:label_recurrence_last) + '</li>' +
+      '<li style="display: inline-block; width: 200px; margin-top: 10px;">' + check_box_tag(param_name, '-1', month_date_checked?(-1)) + ' ' + l(:label_recurrence_last) + '</li>' +
     '</ul>').html_safe
+  end
+
+  def weekday_checked?(day_number)
+    schedule = @issue.planning_schedule
+    if schedule
+      rule = schedule.recurrence_rules.first.try(:to_hash) || Hash.new
+      if rule[:rule_type] == "IceCube::WeeklyRule"
+        rule[:validations][:day].include?(day_number)
+      else
+        false
+      end
+    else
+      false
+    end
   end
 
   def weekdays_ul(param_name)
     ('<ul style = "list-style-type: none;">' +
       weekdays_with_index.map{|i, wd|
-        '<li>' + check_box_tag(param_name, i) + wd + '</li>'
+        '<li>' + check_box_tag(param_name, i, weekday_checked?(i)) + wd + '</li>'
       }.join +
     '</ul>').html_safe
   end
@@ -52,12 +81,32 @@ module RecurringPlanningHelper
   # def monthly_weekdays_options # old
   #   weekdays_ul('validations[day][]')
   # end
+
+  def monthly_weekdays_checked?(week_number, day_number)
+
+  #   rule = @issue.planning_schedule.recurrence_rules.first.to_hash
+  #   rule[:rule_type] == "IceCube::MonthlyRule" && rule[:validations][:day_of_week][week_number].include?(day_number)
+  # rescue
+  #   false
+
+    schedule = @issue.planning_schedule
+    if schedule
+      rule = schedule.recurrence_rules.first.try(:to_hash) || Hash.new
+      if rule[:rule_type] == "IceCube::MonthlyRule"
+        rule[:validations][:day_of_week][week_number].include?(day_number) rescue false
+      else
+        false
+      end
+    else
+      false
+    end
+  end
     
   def monthly_weekdays_options
     (1..4).map{|i|
       "<b style=\"margin-right: 10px;\">#{i}</b>" + 
       weekdays_with_index(:abbr_day_names).map{|j,wd| 
-        check_box_tag("validations[day_of_week][#{j}][]", i, false, style: "margin-left: 15px;") + wd.to_s 
+        check_box_tag("validations[day_of_week][#{j}][]", i, monthly_weekdays_checked?(i,j), style: "margin-left: 15px;") + wd.to_s 
       }.join + '<br>' + '<hr>'
     }.join.html_safe
   end
